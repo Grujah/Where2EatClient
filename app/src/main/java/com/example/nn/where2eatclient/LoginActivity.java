ad60921be23fb89f33802a80780c800c594de334
Grujah <grujah@gmail.com>
2015-09-07 20:16:47 +0200

    login dopola uradje, zajebava nesto

--- app/src/main/java/com/example/nn/where2eatclient/LoginActivity.java
+++ app/src/main/java/com/example/nn/where2eatclient/LoginActivity.java
@@ -1,17 +1,40 @@
 package com.example.nn.where2eatclient;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    ProgressDialog pDialog = new ProgressDialog(this);
+
+    TextView usernameContainer, passwordContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameContainer = (TextView) findViewById(R.id.username_prompt);
+        passwordContainer = (TextView) findViewById(R.id.password_prompt);
+
     }
 
     @Override
@@ -34,5 +57,75 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void beginLogin (View view){
+
+    }
+
+    private class AsyncLogin extends AsyncTask<String,String,String>{
+
+        private final String LOGIN_URL = "192.168.1.5:8081/where2eat/login.php";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // progress dialog make
+            pDialog.setMessage("Conecting to Database");
+            pDialog.show();
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pDialog.hide();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                // build jason
+                JSONObject jason = new JSONObject();
+                jason.accumulate("username", usernameContainer.toString());
+                jason.accumulate("password", passwordContainer.toString());
+
+                String jasonString = jason.toString();
+                StringEntity sEntity = new StringEntity(jasonString);
+
+                //bild Http Client + stuff
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost();
+
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setHeader("Content-type", "application/json");
+
+                // put data in http client
+
+                httpPost.setEntity(sEntity);
+
+                // Send stuff
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+
+                // receive answer
+
+                InputStream inputStream = httpResponse.getEntity().getContent();
+                String answerString = inputStream.toString();
+                JSONObject jasonAnswer = new JSONObject(answerString);
+
+
+
+
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+
+            }
+        return null;
+        }
     }
 }
